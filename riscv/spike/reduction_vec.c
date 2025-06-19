10f92e71bee63303cf27c9c8fe2f9586794aaffc
Gabrocecco <gabrocecco@gmail.com>
2025-06-19 14:22:57 +0200

    First working version of mv_ell_vec()

--- riscv/spike/reduction_vec.c
+++ riscv/spike/reduction_vec.c
@@ -1,14 +1,13 @@
 #include "common.h"
 #include <riscv_vector.h>
 
-/* 
-  Compilation for RISC-V:  
+/*
+  Compilation for RISC-V:
   riscv64-unknown-elf-gcc -O0 -march=rv64gcv -mabi=lp64d -o reduction_vec_rv reduction_vec.c
-  
-  Run on Spike: 
-  spike --isa=rv64gcv pk reduction_vec_rv
-*/ 
 
+  Run on Spike:
+  spike --isa=rv64gcv pk reduction_vec_rv
+*/
 
 // accumulate and reduce
 void reduce_golden(double *a, double *b, double *result_sum,
@@ -34,20 +33,20 @@ void reduce_vec(double *a, double *b, double *result_sum, int *result_count,
 {
   int count = 0;
   // set vlmax and initialize variables
-  size_t vlmax = __riscv_vsetvlmax_e64m1(); // Returns the maximum number of elements of the specified type to process
-  printf("vlmax = %d\n", vlmax);  // every register can hold 2 doubles 
-  vfloat64m1_t vec_zero = __riscv_vfmv_v_f_f64m1(0, vlmax); // create a new vector of vlmax = 2 doubles starting at zeros 
-  vfloat64m1_t vec_s = __riscv_vfmv_v_f_f64m1(0, vlmax);  // --
+  size_t vlmax = __riscv_vsetvlmax_e64m1();                 // Returns the maximum number of elements of the specified type to process
+  printf("vlmax = %ld\n", vlmax);                            // every register can hold 2 doubles
+  vfloat64m1_t vec_zero = __riscv_vfmv_v_f_f64m1(0, vlmax); // create a new vector of vlmax = 2 doubles starting at zeros
+  vfloat64m1_t vec_s = __riscv_vfmv_v_f_f64m1(0, vlmax);    // --
 
   for (size_t vl; n > 0; n -= vl, a += vl, b += vl)
   {
     vl = __riscv_vsetvl_e64m1(n); // set vl at n = 2
-    printf("vl = %d\n", vl);
+    printf("vl = %ld\n", vl);
     printf("n = %d\n", n);
     // printf("a = %d\n", a);
-    // printf("b = %d\n\n", b); 
-    vfloat64m1_t vec_a = __riscv_vle64_v_f64m1(a, vl);  // load vl = 2 values from a array 
-    vfloat64m1_t vec_b = __riscv_vle64_v_f64m1(b, vl);  // --                      b array  
+    // printf("b = %d\n\n", b);
+    vfloat64m1_t vec_a = __riscv_vle64_v_f64m1(a, vl); // load vl = 2 values from a array
+    vfloat64m1_t vec_b = __riscv_vle64_v_f64m1(b, vl); // --                      b array
 
     vbool64_t mask = __riscv_vmfne_vf_f64m1_b64(vec_a, 42, vl); // compare vec_a with scalar 42, create a mask with 0 where vec_a[i]==42
 
@@ -61,6 +60,7 @@ void reduce_vec(double *a, double *b, double *result_sum, int *result_count,
   *result_sum = sum;
   *result_count = count;
 }
+
 
 int main()
 {
@@ -83,7 +83,4 @@ int main()
   // compare
   puts(golden_sum - actual_sum < 1e-6 && golden_count == actual_count ? "pass"
                                                                       : "fail");
-
-
-  
 }
